9db79e5373f1e39e30ae4e94fc2c36dd6d9331c7
codeswitchingstl <codeswitching@icloud.com>
2022-07-08 00:24:35 -0500

    initial commit

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        let pilotName = document.querySelector("input[name=pilotName]");
+        let copilotName = document.querySelector("input[name=copilotName]");
+        let fuelLevel = document.querySelector("input[name=fuelLevel]");
+        let cargoMass = document.querySelector("input[name=cargoMass]");
+    })
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
